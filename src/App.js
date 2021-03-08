6552aa7c86b92d998be60e44e7a43d363f5d7740
Elizabeth Brandt <ebrandt8@uw.edu>
2021-03-07 22:56:44 -0800

    added home, about, projects, and contact routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Nav from './components/Nav';
 import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Switch>
 
           <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/contact" component={Contact} />
 
         </Switch>
       </div>
