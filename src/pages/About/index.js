89d15e614704cd87096f06351f41f11cf1a7092d
Elizabeth Brandt <ebrandt8@uw.edu>
2021-05-05 21:52:06 -0700

    added mini bio

---
+++ src/pages/About/index.js
@@ -0,0 +1,11 @@
+import { Typography } from "@material-ui/core";
+
+function About() {
+    return (
+        <div>
+            <Typography>Full Stack Web Developer utilizing experience in Human Resources, Project Management, and Healthcare to create polished, user-friendly, and organized websites and software platforms. Recently earned a certificate in Full Stack Web Development from the University of Washington Coding Boot Camp. Adaptable problem-solver who is passionate about developing applications that focus on clarity, ease, and efficiency for all users while still providing a powerful tool for their needs. Strengths in planning, process improvement, and leadership.</Typography>
+        </div>
+    )
+}
+
+export default About;
